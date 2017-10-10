44ccdf8c0d8033ff1bab53075ada4e3a2942bafd
diegoerleyduran <diegoerleyduran@yahoo.com>
2017-10-10 16:43:14 -0500

    purebas de regresion

--- app/Post.php
+++ app/Post.php
@@ -1,22 +1,26 @@
 <?php
-
 namespace App;
-
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    protected $fillable = ['title','content'];
+    protected $fillable = ['title', 'content'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+
+      public function getUrlAttribute()
+    {
+        return route('posts.show', [$this->id, $this->slug]);
+    }
 }
-
-
-
-
-
-
--- routes/public.php
+++ routes/public.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
 });
 Auth::routes();
 Route::get('/home', 'HomeController@index');
-Route::get('posts/{post}', [
+Route::get('posts/{post}-{slug}', [
     'as' => 'posts.show',
     'uses' => 'PostController@show'
 ])->where('post', '\d+');
